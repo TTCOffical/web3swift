76d85603396b54909cf5ea7dc29583ff1000bb14
superChan <chenchao@tataufo.com>
2019-12-17 15:30:14 +0800

    self

--- Sources/web3swift/Encryption/keccak.swift
+++ Sources/web3swift/Encryption/keccak.swift
@@ -22,9 +22,9 @@ extension Data {
 //        return data
         
         let nsData = self as NSData
-        let input = nsData.bytes.bindMemory(to: UInt8.self, capacity: data.count)
+        let input = nsData.bytes.bindMemory(to: UInt8.self, capacity: self.count)
         let result = UnsafeMutablePointer<UInt8>.allocate(capacity: 32)
-        keccak_256(result, 32, input, data.count)
+        keccak_256(result, 32, input, self.count)
         
         return Data(bytes: result, count: 32)
     }
