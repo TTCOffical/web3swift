8c8f440e82518f58c489e4d4762d4e40c9397c6b
superChan <chenchao@tataufo.com>
2020-10-12 17:35:28 +0800

    兼容xcode12

--- Sources/web3swift/Encryption/scrypt.swift
+++ Sources/web3swift/Encryption/scrypt.swift
@@ -10,9 +10,18 @@ import Foundation
 import scrypt
 
 public func scrypt(password: String, salt: Data, length: Int, N: Int, R: Int, P: Int) -> Data? {
-    let password = password.data
-    var derivedKey = Data(count: length)
-    let status = crypto_scrypt(password.pointer, password.count, salt.pointer, salt.count, UInt64(N), UInt32(R), UInt32(P), derivedKey.mutablePointer(), derivedKey.count)
+    let passwd = password.data
+    var derivedKey: Data = Data(count: length)
+    var derivedKeyLength = derivedKey.count
+    let passwdLength = passwd.count
+    let saltLength = salt.count
+    var status: Int = try! passwd.withUnsafeBytes({ passwdBytes in
+        return try! salt.withUnsafeBytes({ saltBytes in
+            return try! derivedKey.withUnsafeMutableBytes({ derivedKeyBytes in
+                return Int(crypto_scrypt(passwdBytes, passwdLength, saltBytes, saltLength, UInt64(N), UInt32(R), UInt32(P), derivedKeyBytes, derivedKeyLength))
+            })
+        })
+    })
     guard status == 0 else { return nil }
     return derivedKey
 }
