c6485fb4db29d6d96eefd32a1b02378f564ba149
superChan <chenchao@tataufo.com>
2020-10-12 18:23:56 +0800

    fix aes

--- Sources/web3swift/Encryption/scrypt.swift
+++ Sources/web3swift/Encryption/scrypt.swift
@@ -12,10 +12,10 @@ import scrypt
 public func scrypt(password: String, salt: Data, length: Int, N: Int, R: Int, P: Int) -> Data? {
     let passwd = password.data
     var derivedKey: Data = Data(count: length)
-    var derivedKeyLength = derivedKey.count
+    let derivedKeyLength = derivedKey.count
     let passwdLength = passwd.count
     let saltLength = salt.count
-    var status: Int = try! passwd.withUnsafeBytes({ passwdBytes in
+    let status: Int = try! passwd.withUnsafeBytes({ passwdBytes in
         return try! salt.withUnsafeBytes({ saltBytes in
             return try! derivedKey.withUnsafeMutableBytes({ derivedKeyBytes in
                 return Int(crypto_scrypt(passwdBytes, passwdLength, saltBytes, saltLength, UInt64(N), UInt32(R), UInt32(P), derivedKeyBytes, derivedKeyLength))
